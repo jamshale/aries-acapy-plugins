b1f3b594f0a70f399f6dd0b02d43d6e5593a94b7
jamshale <jamiehalebc@gmail.com>
2024-04-15 15:28:42 -0700

    WIP

--- repo_manager.py
+++ repo_manager.py
@@ -321,7 +321,7 @@ def main(arg_1 = None):
                 os.system(
                     f'cd {plugin_name} && rm poetry.lock && poetry install --all-extras')
                 os.system(
-                    f'cd {plugin_name}/integration && rm poetry.lock && poetry install --all-extras')
+                    f'cd {plugin_name}/integration && rm poetry.lock && poetry install --all-extras --no-root')
                 
     # Install plugin globals
     elif selection == "3":
