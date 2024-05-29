be48487d7f87a637e7e33b805bf4342e00cab6d9
jamshale <jamiehalebc@gmail.com>
2024-05-29 21:37:44 +0000

    Fix formatting
    Signed-off-by: jamshale <jamiehalebc@gmail.com>

--- basicmessage_storage/basicmessage_storage/v1_0/config.py
+++ basicmessage_storage/basicmessage_storage/v1_0/config.py
@@ -69,5 +69,7 @@ def get_config(settings: Mapping[str, Any]) -> BasicMessageStorageConfig:
         config = BasicMessageStorageConfig.default()
 
     LOGGER.debug("Returning config: %s", config.model_dump_json(indent=2))
-    LOGGER.debug("Returning config(aliases): %s", config.model_dump_json(by_alias=True, indent=2))
+    LOGGER.debug(
+        "Returning config(aliases): %s", config.model_dump_json(by_alias=True, indent=2)
+    )
     return config
